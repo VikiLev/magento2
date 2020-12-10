5b9870908fb70a2bea6db3bfa2828861ff1f86c6
VikiLev <vikivikivikikoryagina@gmail.com>
2020-12-10 16:52:52 +0200

    10.12

--- SalesSequenceProfilesEditor/Controller/Adminhtml/Profile/Inlineedit.php
+++ SalesSequenceProfilesEditor/Controller/Adminhtml/Profile/Inlineedit.php
@@ -2,27 +2,73 @@
 
 namespace W4PLEGO\SalesSequenceProfilesEditor\Controller\Adminhtml\Profile;
 
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\SalesSequence\Model\ProfileFactory;
+use Magento\SalesSequence\Model\MetaFactory;
+use W4PLEGO\SalesSequenceProfilesEditor\Model\ResourceModel\Profile\CollectionFactory;
+use Magento\Framework\App\ResourceConnection;
+use Magento\SalesSequence\Model\ResourceModel\Profile as ProfileResourceModel;
+
+/**
+ * Class Inlineedit
+ * @package W4PLEGO\SalesSequenceProfilesEditor\Controller\Adminhtml\Profile
+ */
 class Inlineedit extends \Magento\Backend\App\Action
 {
     /**
-     * @var \Magento\Framework\Controller\Result\JsonFactory
+     * @var JsonFactory
      */
     protected $jsonFactory;
-
-
+    /**
+     * @var ProfileFactory
+     */
     protected $profileFactory;
+    /**
+     * @var Collection
+     */
+    protected $collection;
+    /**
+     * @var ResourceConnection
+     */
+    protected $resource;
+    /**
+     * @var MetaFactory
+     */
+    protected $metaFactory;
+    /**
+     * @var ProfileResourceModel
+     */
+    protected $profileResourceModel;
 
-
+    /**
+     * Inlineedit constructor.
+     * @param Context $context
+     * @param JsonFactory $jsonFactory
+     * @param ProfileFactory $profileFactory
+     * @param MetaFactory $metaFactory
+     * @param CollectionFactory $collection
+     * @param ResourceConnection $resource
+     * @param ProfileResourceModel $profileResourceModel
+     */
     public function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory,
-        \Magento\SalesSequence\Model\ProfileFactory $profileFactory
-    ) {
+        Context $context,
+        JsonFactory $jsonFactory,
+        ProfileFactory $profileFactory,
+        MetaFactory $metaFactory,
+        CollectionFactory $collection,
+        ResourceConnection $resource,
+        ProfileResourceModel $profileResourceModel
+    )
+    {
         parent::__construct($context);
+        $this->profileResourceModel = $profileResourceModel;
+        $this->resource = $resource;
+        $this->collection = $collection;
         $this->jsonFactory = $jsonFactory;
         $this->profileFactory = $profileFactory;
+        $this->metaFactory = $metaFactory;
     }
-
 
     /**
      * Inline edit action
@@ -35,7 +81,6 @@ class Inlineedit extends \Magento\Backend\App\Action
         $resultJson = $this->jsonFactory->create();
         $error = false;
         $messages = [];
-
         if ($this->getRequest()->getParam('isAjax')) {
             $postItems = $this->getRequest()->getParam('items', []);
             if (!count($postItems)) {
@@ -45,9 +90,38 @@ class Inlineedit extends \Magento\Backend\App\Action
                 foreach (array_keys($postItems) as $modelid) {
                     $model = $this->profileFactory->create()->load($modelid);
                     try {
-                        $model->setData(array_merge($model->getData(), $postItems[$modelid]));
-                        $model->save();
-                    } catch (\Exception $e) {
+                        $prefix = $postItems[$modelid]['prefix'];
+                        $suffix = $postItems[$modelid]['suffix'];
+                        $profileId = $postItems[$modelid]['profile_id'];
+                        $profile=$this->profileFactory->create()->load($profileId);
+                        $prefixById = $profile->getData('prefix');
+                        $suffixById = $profile->getData('suffix');
+                        $metaId = $profileId;
+                        $entityType = $this->metaFactory->create()->load($metaId)->getData('entity_type');
+                        $second_table_name = $this->resource->getTableName('sales_sequence_meta');
+                        $collection = $this->collection->create()
+                            ->join([$second_table_name], "main_table.meta_id =$second_table_name.meta_id")
+                            ->addFieldToFilter('entity_type', $entityType);
+                        $prefixTab = [];
+                        $suffixTab = [];
+                        foreach ($collection as $item) {
+                            if (!empty($item->getData('prefix'))) {
+                                $prefixTab[] = $item->getData('prefix');
+                            }
+                            if (!empty($item->getData('suffix'))) {
+                                $suffixTab[] = $item->getData('suffix');
+                            }
+                        }
+                        if (in_array($prefix, $prefixTab) && !($prefixById == $prefix) || in_array($suffix, $suffixTab) && !($suffixById == $suffix)) {
+                            $messages[] = __($entityType . ' with this number already exists');
+                            $error = true;
+                        } else {
+                            $model->setData(array_merge($model->getData(), $postItems[$modelid]));
+                            $this->profileResourceModel->save($model);
+
+                        }
+                    } catch
+                    (\Exception $e) {
                         $messages[] = "[File Icon ID: {$modelid}]  {$e->getMessage()}";
                         $error = true;
                     }
@@ -61,3 +135,4 @@ class Inlineedit extends \Magento\Backend\App\Action
         ]);
     }
 }
+
